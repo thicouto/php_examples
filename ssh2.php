be10fb0d30e65cc1f836149deca47d1f212a4ed1
Thiago Couto <66148867+thicouto@users.noreply.github.com>
2021-04-05 16:20:58 -0300

    Update ssh2.php

--- ssh2.php
+++ ssh2.php
@@ -1,8 +1,8 @@
 <?php
 $string_stream = "/usr/bin/python/find_tag.py"; // Command to be executed.
-$connection = ssh2_connect("localhost", "22");
-ssh2_auth_password($connection, "pi", "_Prod_654_123");
-$stream = ssh2_exec($connection, $string_stream);
+$connection = ssh2_connect("localhost", "22"); // ip and port
+ssh2_auth_password($connection, "pi", "_Prod_654_123"); // user and password
+$stream = ssh2_exec($connection, $string_stream); // execute
 stream_get_contents($stream); // Wait for command to finish.
-fclose($stream);
+fclose($stream); // close
 ?>
